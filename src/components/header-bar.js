ecf474cc4402d10a7789989cfd75f21802347840
Janet Leon <leon.irenejanet@gmail.com>
2018-10-13 19:20:33 -0700

    typo fix

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -4,8 +4,8 @@ import './css/header-bar.css'
 
 export default function HeaderBar(props){
     return(
-        <header class="content-header flex-center">
-      <h1 class="section-title">News Flash</h1>
+        <header className="content-header flex-center">
+      <h1 className="section-title">News Flash</h1>
     </header>
     );
 }
