0b9e237aa8d67e51a158f1b8a58e1d9fbe857622
Janet Leon <leon.irenejanet@gmail.com>
2018-10-13 22:42:02 -0700

    testing

---
+++ src/components/side-nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './css/side-nav.css';
+export default function SideNav (props){
+    return(  
+      <nav class="site-nav flex-center">
+      <h1 class="section-title">NAV</h1>
+    </nav>
+  )
+}
