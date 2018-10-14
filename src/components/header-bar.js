bdd84108dc2866d983aa4f4945b5d60b2cc451b0
Janet Leon <leon.irenejanet@gmail.com>
2018-10-13 19:19:33 -0700

    header and nav components

---
+++ src/components/header-bar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/header-bar.css'
+
+
+export default function HeaderBar(props){
+    return(
+        <header class="content-header flex-center">
+      <h1 class="section-title">News Flash</h1>
+    </header>
+    );
+}
+
+
---
+++ src/components/side-nav.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './css/side-nav.css';
+export default function SideNav (props){
+    return(  
+    <nav className="site-nav flex-center">
+    <h1 className="section-title">NAV</h1>
+  </nav>)
+}
