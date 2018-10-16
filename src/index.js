8fd1d78b7fad918788b34c81c06abe169bd5277a
Janet Leon <leon.irenejanet@gmail.com>
2018-10-16 15:39:44 -0700

    set up app with authentication boilderplate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +9,9 @@ import store from './store'
 
 ReactDOM.render(
 <Provider store={store}>
+<Router>
 <App />
+</Router>
 </Provider>,
 document.getElementById('root'));
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,16 @@ import categoryReducer from '../reducers/category-reducer';
 import searchReducer from '../reducers/search-reducer';
 import navReducer from './nav-reducer';
 import userMenuReducer from './userMenu-reducer';
+import authReducer from './auth';
+import protectedDataReducer from './protected-data';
 
 const rootReducer = combineReducers({
     category: categoryReducer,
     search: searchReducer,
     nav: navReducer,
-    userMenu: userMenuReducer
+    userMenu: userMenuReducer,
+    auth: authReducer,
+    protectedData: protectedDataReducer
 })
 
 export default rootReducer;
