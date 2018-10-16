211bbb0386e4cfb3831f4b53d539f8dbdd581c20
Janet Leon <leon.irenejanet@gmail.com>
2018-10-16 16:01:41 -0700

    REGISTRATION WORKING :-)

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,10 +1,10 @@
 import {SubmissionError} from 'redux-form';
 
-import {API_BASE_URL} from '../config';
+import {SERVER} from '../config';
 import {normalizeResponseErrors} from './utils';
 
 export const registerUser = user => dispatch => {
-    return fetch(`${API_BASE_URL}/users`, {
+    return fetch(`${SERVER}/users`, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
