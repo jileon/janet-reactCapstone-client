d29acb03a5f5217284ac382f1584445c58de509a
Janet Leon <leon.irenejanet@gmail.com>
2018-10-16 14:37:51 -0700

    modified base url

--- src/components/search.js
+++ src/components/search.js
@@ -15,7 +15,7 @@ class SearchForm extends React.Component{
                     e.preventDefault()
                     searchTerm=this.input.value;
                         this.props.dispatch(setSearchTerm(this.input.value));
-                        this.props.dispatch(getSearchTerm(searchTerm, 2));
+                        this.props.dispatch(getSearchTerm(searchTerm, 1));
                     }}>
 
                     Search
