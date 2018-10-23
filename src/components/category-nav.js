fc62bc2ef945ea3b50b72a295dc2a7b0c7cf724b
Janet Leon <leon.irenejanet@gmail.com>
2018-10-23 16:54:00 -0700

    fixed button issue

--- src/components/category-nav.js
+++ src/components/category-nav.js
@@ -3,14 +3,27 @@ import './css/category-nav.css'
 
 export default function CategoryNav(props){
     return(
-        <ul className='category-flex-container'>    
-            <li><button className='catagory-nav' type='button' name='headlines' onClick={props.buttonClick}>Everything</button></li>
+        <ul className='category-flex-container'> 
+        <div>
+            
+        <li><button className='catagory-nav' type='button' name='headlines' onClick={props.buttonClick}>Everything</button></li>
             <li><button className='catagory-nav' type='button' name='entertainment' onClick={props.buttonClick}>Entertainment</button></li>
-            <li><button className='catagory-nav' type='button' name='business' onClick={props.buttonClick}>Business</button></li>
+           
+            </div>  
+
+<div>
+<li><button className='catagory-nav' type='button' name='business' onClick={props.buttonClick}>Business</button></li>
             <li><button className='catagory-nav' type='button' name='health' onClick={props.buttonClick}>Health</button></li>
-            <li><button className='catagory-nav' type='button' name='science' onClick={props.buttonClick}>Science</button></li>
+
+</div>
+
+
+         <div>
+         <li><button className='catagory-nav' type='button' name='science' onClick={props.buttonClick}>Science</button></li>
             <li><button className='catagory-nav' type='button' name='sports'onClick={props.buttonClick}>Sports</button></li>
-            <li><button className='catagory-nav' type='button' name='technology' onClick={props.buttonClick}>Technology</button></li>
+          
+         </div>
+         <li><button className='catagory-nav' type='button' name='technology' onClick={props.buttonClick}>Technology</button></li>
         </ul>
    
     )
