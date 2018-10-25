c3339c9c851ca9290f2ff0d5eca55b4bac689906
Janet Leon <leon.irenejanet@gmail.com>
2018-10-24 17:17:46 -0700

    added text and styling to modal

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -13,6 +13,7 @@ import MainSection from './main-section';
 import CategoryNav from './category-nav';
 import ReactModal from 'react-modal';
 import '../App.css';
+import './css/modal.css'
 
 
 
@@ -50,8 +51,26 @@ export class Dashboard extends React.Component {
 						onRequestClose={this.handleCloseModal}
 						shouldCloseOnOverlayClick={false}
 						className="Modal">
-						<p>Modal text!</p>
+						<section className='about-newsflash'>
+						<h1>About Newsflash</h1>
+						<p>Newsflash to save and get updated news for topics that interest you.
+							You can browse headlines by category using the buttons at the top of the header.</p>
+							<p>You can save an article by creating customized folders. To create a new folder, click on the side navigation bar to open the folder display.
+							Create a new folder by typing a name in the input and press enter. Go back to the main page, hover over an article you'd like to save, 
+							and you will see a button pop up called 'Add to Folder'. Hover over that button to see a drop down menu of all your folders.</p>
+
+							<p>To view what's in your folders, click on the side navigation bar. You can see a card view list of all your folders. Click on each folder to open up 
+							a page with every article saved to that folder. When you decide you no longer need that article, delete it by hovering over the article card and click
+							the delete button at the top.</p>
+							
+							
+
+
+						</section>
+					
+						<div className='modal-close'>
 						<button onClick={()=>{this.props.dispatch(modalOff())}}>Got It</button>
+						</div>
 					</ReactModal>
 				</section>
 			</div>
