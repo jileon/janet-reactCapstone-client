edffc07a8054523b7c1c79a24fc830f5f08256b0
Janet Leon <leon.irenejanet@gmail.com>
2018-10-26 10:03:24 -0700

    changed about

--- src/components/about.js
+++ src/components/about.js
@@ -3,14 +3,14 @@ import React from 'react';
 export default function About(props) {
 	return (
 		<section className="about-newsflash">
-			<h1>About Newsflash</h1>
+			<h1>About News flash</h1>
 			<p>
-				Newsflash to save and get updated news for topics that interest you. You can browse headlines by
-				category using the buttons at the top of the header.
+				News flash is a way to save and get updated news for topics that interest you. You can browse headlines by
+				category using the buttons at the top navigation.
 			</p>
 			<p>
 				You can save an article by creating customized folders. To create a new folder, click on the side
-				navigation bar to open the folder display. Create a new folder by typing a name in the input and press
+				navigation bar to open the folder display menu. Create a new folder by typing a name in the input and press
 				enter. Go back to the main page, hover over an article you'd like to save, and you will see a button pop
 				up called 'Add to Folder'. Hover over that button to see a drop down menu of all your folders.
 			</p>
