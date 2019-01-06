6bb6bfae458ad0407cf547d51deaa9e82c3eb6c9
Janet Leon <ajleon00@gmail.com>
2019-01-05 16:18:30 -0800

    completes skeleton for landing page

--- src/components/Landing-Page/index.js
+++ src/components/Landing-Page/index.js
@@ -14,17 +14,67 @@ export class LandingPage extends React.Component {
         <div className="intro-hero" />
         <div className="intro-text">
           <h1>
-            Welcome to Newsflash<span className="period">.</span>
-            <button>Learn More</button>
+            Welcome <br /> to Newsflash<span className="period">.</span>
           </h1>
+          <div className="intro-controls">
+            <a href="#newsflash">
+              {' '}
+              <button type="button">Learn More</button>
+            </a>
+            <button type="button">Login</button>
+          </div>
         </div>
         <HeaderBarBlack class="header-title-black" />
 
-        <div className="about-newsflash" />
+        <section className="about-newsflash-intro">
+          <div id="newsflash" name="newsflash" className="scroll-div" />
+          <h1>
+            Organize Your Reading Lists<span className="period">.</span>
+          </h1>
+          <div className="about-newsflash-txt">
+            <p>
+              Jumping around sources and websites on different tabs is
+              frustrating. Newsflash lets you browse different articles from
+              multiple sources all in one place. If you want to save an article
+              to read later, simply create a folder to save your article in and
+              come back to it when you're ready. No need to bookmark everything
+              only to get lost in a disorganized void.
+            </p>
+            <div className="txt-controls ">
+              <a href="#newsflash">
+                <button type="button">Signup</button>
+              </a>
+              <button type="button">Try a Demo</button>
+            </div>
+          </div>
 
-        {/* <Link to="/login">
-          <button>Login</button>
-        </Link> */}
+          <div className="about-how-to">
+            <h1>
+              Save your articles in one place<span className="period">.</span>
+            </h1>
+            <div>
+              <p>
+                Instead of having everything scrambles together, customize your
+                reading lists to your needs.
+              </p>
+            </div>
+            <div className="how-to-gif">
+              <img src={require('../../images/howto3.gif')} />
+            </div>
+          </div>
+          <div className="landing-footer">
+            <p>
+              <a
+                href="https://iamjanetleon.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Built and Designed by Janet Leon
+              </a>
+              <br /> <br /> Powered by news.api.org
+            </p>
+          </div>
+        </section>
       </section>
     );
   }
